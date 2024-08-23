54b0891ccc5c45e0071e02849332e5fefef17bfd
Denis Svichkarev <denis.svichkarev@gmail.com>
2024-08-23 17:56:10 +0200

    Created SwiftUI project with SwiftData sample

---
+++ Sample Project SwiftUI/Sample Project SwiftUIUITests/Sample_Project_SwiftUIUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Sample_Project_SwiftUIUITestsLaunchTests.swift
+//  Sample Project SwiftUIUITests
+//
+//  Created by Denis Svichkarev on 23/08/24.
+//
+
+import XCTest
+
+final class Sample_Project_SwiftUIUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
